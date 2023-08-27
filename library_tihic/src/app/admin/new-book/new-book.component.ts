0a04d2da585704f6d4843e38bf984a41efcbc86b
Anja Tihic <atihic99@gmail.com>
2023-08-27 12:53:04 +0200

    Make genre dropdown functional

--- library_tihic/src/app/admin/new-book/new-book.component.ts
+++ library_tihic/src/app/admin/new-book/new-book.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Genre } from 'src/app/models/genre.model';
+import { GenreService } from 'src/app/services/genre.service';
 
 @Component({
   selector: 'app-new-book',
   templateUrl: './new-book.component.html',
   styleUrls: ['./new-book.component.css']
 })
-export class NewBookComponent {
+export class NewBookComponent implements OnInit {
+  allGenres: Genre[] = [];
 
+  constructor(private genreService: GenreService) {}
+
+  ngOnInit(): void {
+    this.genreService.getAllGenres();
+    this.genreService.allGenresSub.subscribe( genres => {
+      this.allGenres = genres;
+    })
+  }
 }
