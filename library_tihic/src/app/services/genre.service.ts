0d12a82d535a8d308e8cfea3216d3f8e7e1c1a7c
Anja Tihic <atihic99@gmail.com>
2023-08-27 12:38:29 +0200

    Create getAllGenres method

--- library_tihic/src/app/services/genre.service.ts
+++ library_tihic/src/app/services/genre.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Genre } from '../models/genre.model';
 import {Subject} from "rxjs";
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,34 @@ import {Subject} from "rxjs";
 export class GenreService {
 
   error = new Subject<string>();
+  DATABASE_URL = "https://angular-library-8b92a-default-rtdb.firebaseio.com/genre.json"
+
+  allGenresSub = new Subject<Genre[]>();
+  allGenres: Genre[] = [];
 
   constructor(private http: HttpClient) { }
 
   createNewGenre(name: string, about: string){
     const genreData = new Genre(name, about);
 
-    this.http.post('https://angular-library-8b92a-default-rtdb.firebaseio.com/genre.json', genreData)
+    this.http.post(this.DATABASE_URL, genreData)
       .subscribe(response => {
         console.log(response);
       }, error => {
         this.error.next(error.message);
       })
   }
+
+  getAllGenres(){
+
+    this.http.get(this.DATABASE_URL)
+      .subscribe((res: any) => {
+        for(let key in res){
+          this.allGenres.push({...res[key], id: key});
+        }
+
+        this.allGenresSub.next(this.allGenres);
+      })
+  }
+
 }
