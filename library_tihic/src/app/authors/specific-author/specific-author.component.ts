75a97f0e9ad5cec973ed31d597ec8f34badcf2bf
Anja Tihic <atihic99@gmail.com>
2023-08-28 11:49:40 +0200

    Design author page

--- library_tihic/src/app/authors/specific-author/specific-author.component.ts
+++ library_tihic/src/app/authors/specific-author/specific-author.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Author } from 'src/app/models/author.model';
+import { Book } from 'src/app/models/book.model';
+import { AuthorService } from 'src/app/services/author.service';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-specific-author',
   templateUrl: './specific-author.component.html',
   styleUrls: ['./specific-author.component.css']
 })
-export class SpecificAuthorComponent {
+export class SpecificAuthorComponent implements OnInit{
+  loadedAuthor = new Author();
+  booksOfAuthor: Book[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private authorService: AuthorService,
+    private bookService: BookService
+  )  {}
+
+  ngOnInit(): void {
+    let authorId = this.route.snapshot.params['id'];
+    this.authorService.getAuthorById(authorId).subscribe( res => {
+      this.loadedAuthor = res;
+    })
+
+    this.bookService.getAllBooks().subscribe (res => {
+      this.booksOfAuthor = res.filter(book => book.authorId == authorId);
+    })
+  }
 
 }
