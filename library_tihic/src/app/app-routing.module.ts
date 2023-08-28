82e0fa0de67373e91b31ffc9f3f3f5f6f8bf0cbe
Anja Tihic <atihic99@gmail.com>
2023-08-28 11:30:10 +0200

    Fix route

--- library_tihic/src/app/app-routing.module.ts
+++ library_tihic/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {path: 'genres', component: GenresComponent},
   {path: 'genres/:id', component: SpecificGenreComponent},
   {path: 'authors', component: AuthorsComponent},
-  {path: 'authors/id', component: SpecificAuthorComponent}
+  {path: 'authors/:id', component: SpecificAuthorComponent}
 ];
 
 @NgModule({
