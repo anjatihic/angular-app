9a6f90ed97afb68172071c7c96e4510e0c9e319a
Anja Tihic <atihic99@gmail.com>
2023-08-28 10:42:13 +0200

    Separate components

--- library_tihic/src/app/app.module.ts
+++ library_tihic/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NewBookComponent } from './admin/new-book/new-book.component';
 import { NewGenreComponent } from './admin/new-genre/new-genre.component';
 import { GenresComponent } from './genres/genres.component';
 import { SpecificGenreComponent } from './genres/specific-genre/specific-genre.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SpecificGenreComponent } from './genres/specific-genre/specific-genre.c
     NewBookComponent,
     NewGenreComponent,
     GenresComponent,
-    SpecificGenreComponent
+    SpecificGenreComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
