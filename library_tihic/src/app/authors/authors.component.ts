16a272a94e2343b6c0e1ab9824707c30f6410a5e
Anja Tihic <atihic99@gmail.com>
2023-08-28 11:29:09 +0200

    Design the page

--- library_tihic/src/app/authors/authors.component.ts
+++ library_tihic/src/app/authors/authors.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Author } from '../models/author.model';
+import { AuthorService } from '../services/author.service';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
-export class AuthorsComponent {
+export class AuthorsComponent implements OnInit{
+
+  allAuthors: Author[] = [];
+
+  constructor(private authorService: AuthorService) {}
+
+  ngOnInit(): void {
+    this.authorService.getAllAuthors().subscribe(res => {
+      this.allAuthors = res;
+    })
+  }
 
 }
